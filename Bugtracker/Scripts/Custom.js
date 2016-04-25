dc6374c73504963f3705c82bb20a2fc33803d243
danlor6808 <danlor6808@gmailcom>
2016-04-25 11:28:24 -0400

    4/25/16

--- Bugtracker/Scripts/Custom.js
+++ Bugtracker/Scripts/Custom.js
@@ -1,14 +1,6 @@
 ﻿$(document).ready(function() {
 
     $('#datetimepicker').datetimepicker({
-        dateFormat: "dd/mm/yy",
-        showStatus: true,
-        showWeeks: true,
-        currentText: 'Now',
-        autoSize: true,
-        gotoCurrent: true,
-        showAnim: 'blind',
-        highlightWeek: true
     });
     $('select').selectpicker();
     $("#phone").mask("(999) 999-9999");
